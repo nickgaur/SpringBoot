1c2368fdec0d6045f9b45ffc4ec0be78f2dd4f33
Nick Gaur <nikhilgaur581@gmail.com>
2024-08-30 23:02:10 +0530

    .

---
+++ src/main/java/com/in28minutes/learn_spring_framework/GamingAppBasicJava.java
@@ -0,0 +1,10 @@
+package com.in28minutes.learn_spring_framework;
+
+public class GamingAppBasicJava {
+
+	public static void main(String [] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
